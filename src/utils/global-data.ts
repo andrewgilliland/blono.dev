6bb2fc958346122ae0b75b877b95a68eeaeb5436
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-09-09 10:35:36 -0500

    feat: updates getGlobalData and README

--- src/utils/global-data.ts
+++ src/utils/global-data.ts
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Bloomington-Normal Developers';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Blomington-Normal Developers';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
