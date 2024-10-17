3ec1f7b0c258cb12bff97748b406d46b3b30829b
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-10-17 10:05:03 -0400

    feat: updates EventCard ui

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -19,21 +19,16 @@ const EventCard: FC<EventCardProps> = ({ event }) => {
       <div className="absolute -top-2 -left-2 border h-full w-full group-hover:translate-x-2 group-hover:translate-y-2 transition-transform duration-1000" />
       <div className="relative flex flex-col-reverse md:flex-row bg-white border">
         <div className="flex flex-col md:flex-row px-6 py-10 gap-10">
-          <div className="flex md:flex-col justify-between md:justify-start items-baseline md:items-start gap-3 text-gray-700 min-w-max">
-            <div className="font-bold text-base">
-              {dayOfWeek} {month} {day}
-            </div>
-            <div className="font-semibold text-sm">
-              {startTime} - {endTime}
-            </div>
-          </div>
           <div className="flex flex-col gap-3 items-start max-w-2xl">
-            <h3 className="text-heading-tertiary">{title}</h3>
-            <div
-              className={`bg-purple-500 font-semibold text-white text-xs px-2 py-1`}
-            >
-              {location}
+            <div className="flex flex-wrap gap-3">
+              <div className="bg-purple-500 font-semibold text-white text-xs px-2 py-1">
+                {dayOfWeek}, {month} {day} · {startTime} - {endTime} CST
+              </div>
+              <div className="bg-gray-200 font-semibold text-gray-500 text-xs px-2 py-1">
+                {location}
+              </div>
             </div>
+            <h3 className="text-heading-tertiary">{title}</h3>
             <div className="text-copy">{details}</div>
           </div>
         </div>
