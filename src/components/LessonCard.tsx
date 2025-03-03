22ed383bcf5ca370323bfec1e5106fb065b30436
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-03-03 16:28:22 -0600

    feat: updates LessonCard

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -17,15 +17,15 @@ const LessonCard: FC<LessonCardProps> = ({ lesson }) => {
   const { title, description, slug, number } = lesson;
 
   return (
-    <div className="relative px-8 py-10 h-full bg-purp border border-light border-opacity-75 rounded-[10px]">
-      <h2 className="text-green-500 font-bold text-2xl">Lesson {number}</h2>
-      <h3 className="text-white">{title}</h3>
+    <div className="relative px-8 py-10 h-full bg-darker border border-light border-opacity-75 rounded-[10px]">
+      <h2 className="text-copy">Lesson {number}</h2>
+      <h3 className="text-heading-tertiary">{title}</h3>
       <div className="flex flex-col gap-4 mt-8">
         <Link
           href={`/workshops/react-fundamentals/${slug}`}
           className="text-white font-semibold text-lg"
         >
-          <Button theme="green" size="md" className="mt-6">
+          <Button theme="purp" size="md" className="mt-6">
             Get Started
           </Button>
         </Link>
