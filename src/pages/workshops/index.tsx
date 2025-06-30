27a70088d8847ab29686ee2b9966e6c7918fa5b7
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-06-29 20:06:44 -0500

    refactor: adds WorkshopsHeroSection

--- src/pages/workshops/index.tsx
+++ src/pages/workshops/index.tsx
@@ -1,43 +1,13 @@
-import Circle from "@/components/icons/Circle";
 import Container from "@/components/layout/Container";
 import Layout from "@/components/layout/Layout";
 import ContactSection from "@/components/sections/ContactSection";
 import WorkshopCard from "@/components/sections/WorkshopCard";
+import WorkshopsHeroSection from "@/components/sections/WorkshopsHeroSection";
 
 const WorkshopsPage = () => {
   return (
     <Layout>
-      <section id="hero">
-        <Container>
-          <div className="flex justify-center items-center flex-col md:flex-row mt-20 gap-12 md:gap-20">
-            <div className="relative">
-              <Circle
-                size={434}
-                className="absolute fill-purp-dark -translate-x-[325px] -translate-y-[325px] opacity-20"
-              />
-              <Circle
-                size={100}
-                className="absolute fill-purp-dark translate-x-32 -translate-y-12 opacity-20"
-              />
-              <Circle
-                size={100}
-                className="absolute fill-purp-dark translate-x-36 translate-y-20 opacity-20"
-              />
-              <h1 className="relative">Workshops</h1>
-            </div>
-            <div className="relative group flex w-full md:w-1/2">
-              <Circle
-                size={250}
-                className="absolute fill-theme-indigo -translate-x-5 md:-translate-y-20 opacity-20"
-              />
-              <p className="relative font-semibold text-2xl text-white md:mt-10">
-                Get some hands on programming knowledge with Bloomington-Normal
-                Developer&apos;s workshops.
-              </p>
-            </div>
-          </div>
-        </Container>
-      </section>
+      <WorkshopsHeroSection />
       <section id="workshops">
         <Container className="mt-16">
           <WorkshopCard />
