18ed02bd1fe359aa231650dec3d06e6e88def51b
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-06-29 21:33:27 -0500

    refactor: updates .prettierrc.json for tw prettier plugin

--- src/components/sections/UpcomingEventsSection/index.tsx
+++ src/components/sections/UpcomingEventsSection/index.tsx
@@ -1,9 +1,9 @@
-import { FC } from "react";
-import { EventType } from "@/types";
-import Circle from "@/components/icons/Circle";
-import EventCard from "../EventCard";
-import Container from "@/components/layout/Container";
-import { SECTION_TOP_PADDING_OFFSET } from "@/lib/constants";
+import { FC } from 'react';
+import { EventType } from '@/types';
+import Circle from '@/components/icons/Circle';
+import EventCard from '../EventCard';
+import Container from '@/components/layout/Container';
+import { SECTION_TOP_PADDING_OFFSET } from '@/lib/constants';
 
 type EventSectionProps = {
   events: EventType[];
@@ -14,7 +14,7 @@ const UpcomingEventsSection: FC<EventSectionProps> = ({ events }) => {
 
   const upcomingEvents = events.filter((event) => new Date(event.date) > now);
   upcomingEvents.sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
   );
 
   return (
@@ -25,18 +25,18 @@ const UpcomingEventsSection: FC<EventSectionProps> = ({ events }) => {
             <div>
               <Circle
                 size={250}
-                className="absolute fill-purp-dark -translate-x-24 -translate-y-24 opacity-20"
+                className="absolute -translate-x-24 -translate-y-24 fill-purp-dark opacity-20"
               />
               <Circle
                 size={100}
-                className="absolute fill-purp-dark translate-x-40 translate-y-12 opacity-20"
+                className="absolute translate-x-40 translate-y-12 fill-purp-dark opacity-20"
               />
               <Circle
                 size={100}
-                className="absolute fill-purp-dark translate-x-64 -translate-y-12 opacity-20 hidden md:block"
+                className="absolute hidden -translate-y-12 translate-x-64 fill-purp-dark opacity-20 md:block"
               />
             </div>
-            <h2 className="relative text-heading-secondary mb-12">
+            <h2 className="text-heading-secondary relative mb-12">
               Upcoming Events
             </h2>
           </div>
